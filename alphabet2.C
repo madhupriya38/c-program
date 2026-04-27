d8442a7649605546116d9cc12f845bbbfdc1263b
madhupriya38 <madhumatcha13@gmail.com>
2026-04-27 13:12:08 +0530

    Add files via upload

---
+++ alphabet2.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("enter the character\n");
+    scanf("%c",&ch);
+     int res=(ch>='A')&&(ch<='Z')||(ch>='a')&&(ch<='z');
+     printf("%c is a alphabet i.e is :%d",ch,res);
+
+}
